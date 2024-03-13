edc0ca51e72d2f5daf9d1e54fcb1a9f04c5da9d6
mutiaradevita <mutiaradevitaekaputri>
2024-03-13 14:12:30 +0700

    praktikum 2

--- event-and-state/src/components/button.tsx
+++ event-and-state/src/components/button.tsx
@@ -1,3 +1,12 @@
+export function Tombol_2({isiPesan, namaTombol} : {isiPesan:any, namaTombol:any}) {
+	return (
+		<button 
+            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+			onClick={() => alert(isiPesan)}>
+			{namaTombol}
+		</button>
+	);
+    }
 export default function Tombol_1() {
     // menambahkan fungsi untuk menangani klik tombol
     function handleClick() {
